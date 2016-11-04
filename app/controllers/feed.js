4d8c26808a3117e5f977ad3c83afc0fb45a7c8f8
Eric Kelly <heroiceric@gmail.com>
2016-11-03 23:41:38 -0700

    Routing - Step 3
    Here we create a Feed controller. The best way to think about a
    Controller is that it is like the highest level component for a route.
    
    We add an `{{outlet}}` to the application route because that's how we
    say where to render child routes. The application is the highest level
    route so our `feed` route will be rendered inside the `{{outlet}}` of
    the application route.
    
    We need to update our link in the header because we're overriding the
    default `index` route when we set the `feed` route's path to `'/'`.
    
    Support for converting JavaScript objects to inline styles isn't build
    in but there are some easy ways to do that. I'm just going to ignore
    that for now because I don't feel like it's important at the moment and
    we can revisit that later :-p

---
+++ app/controllers/feed.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+const { Controller } = Ember;
+
+export default Controller.extend({
+  styles: {
+    title: 'text-align: center;',
+    tweets: 'margin-top: 32px;'
+  }
+});
--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('feed', { path: '/' });
 });
 
 export default Router;
